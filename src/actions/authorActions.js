dae75c073e0bbb29d42520265dd863f3c1b42403
ruben.perez <ruben28693@gmail.com>
2018-10-08 17:03:21 +0200

    Hide preloader

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,12 +1,14 @@
 import * as types from "./actionTypes";
 import AuthorApi from '../api/mockAuthorApi';
+import {beginAjaxCall} from "./ajaxStatusActions";
 
 export function loadAuthorsSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
 }
 
 export function loadAuthors() {
-  return function (dispatch) {
+  return dispatch => {
+    dispatch(beginAjaxCall());
     return AuthorApi.getAllAuthors()
       .then(authors => {
         dispatch(loadAuthorsSuccess(authors));
--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
 import React, {Component, PropTypes} from "react";
 import Header from "./common/Header";
+import {connect} from "react-redux";
 
 class App extends Component {
   render() {
     return (
       <div className="container-fluid">
-        <Header/>
+        <Header loading={this.props.loading}/>
         {this.props.children}
       </div>
     );
   }
 }
 
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.ajaxCallsInProgress > 0
+  };
+}
+
 App.propTypes = {
-  children: PropTypes.object.isRequired
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool
 };
 
-export default App;
+export default connect(mapStateToProps)(App);
