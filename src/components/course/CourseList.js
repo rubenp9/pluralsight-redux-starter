4e90081f4f5a718bdc5eb8d9b206cd87f9fa196d
ruben.perez <ruben28693@gmail.com>
2018-10-08 10:31:45 +0200

    Create Course List Component

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -3,14 +3,14 @@ import CourseListRow from "./CourseListRow";
 
 const CourseList = ({courses}) => {
   return (
-    <table>
+    <table className="table">
       <thead>
       <tr>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th>&nbsp;</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Category</th>
+        <th>Length</th>
       </tr>
       </thead>
       <tbody>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from "./routes";
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from "./store/configureStore";
 import {loadCourses} from "./actions/courseActions";
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const store = configureStore();
 store.dispatch(loadCourses());
